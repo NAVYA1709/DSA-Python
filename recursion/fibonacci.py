d5b10bc99a6f272eebbd574f95ebc472cbc912c1
Navyasri <navyasrikothuri@gmail.com>
2026-06-06 22:05:12 +0530

    Create fibonacci.py

---
+++ recursion/fibonacci.py
@@ -0,0 +1,17 @@
+# Problem: Fibonacci Number
+# Approach: Recursion
+# Time Complexity: O(2^n)
+# Space Complexity: O(n)
+
+def fib(n):
+
+    if n == 0:
+        return 0
+
+    if n == 1:
+        return 1
+
+    return fib(n - 1) + fib(n - 2)
+
+
+print(fib(6))
